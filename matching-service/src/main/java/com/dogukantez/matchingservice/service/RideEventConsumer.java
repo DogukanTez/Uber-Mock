5147bf018bd2eaae8902a4df70099f0c27733337
Doğukan Tez <dogukantez7@gmail.com>
2026-06-23 10:41:40 +0300

    match service ride event consumer

--- matching-service/src/main/java/com/dogukantez/matchingservice/service/RideEventConsumer.java
+++ matching-service/src/main/java/com/dogukantez/matchingservice/service/RideEventConsumer.java
@@ -1,4 +1,40 @@
 package com.dogukantez.matchingservice.service;
 
+import com.dogukantez.matchingservice.event.RideRequestedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class RideEventConsumer {
+    private final MatchingService matchingService;
+
+    public RideEventConsumer(MatchingService matchingService) {
+        this.matchingService = matchingService;
+    }
+
+    /**
+     * Listens ride.requested kafka topic.
+     * Triggered every time Ride Service published a new ride request
+     * FLOW:
+     * Ride Service -> Kafka (ride.requested) -> This Consumer -> MatchingService
+     */
+
+    @KafkaListener(
+            topics = "ride.requested",
+            groupId = "matching-service-group"
+    )
+    public void consumeRideRequestedEvent(RideRequestedEvent event){
+        try{
+            matchingService.matchDriverForRide(event);
+        }
+        catch (Exception e){
+            log.error("Error processing ride request: {} - {}",
+                    event.getRideId(), e.getMessage());
+
+            // In production: send to dead letter queue for retry
+        }
+    }
+
 }
